b53698d5b2dc579063b9c976722cd8027f9d1ee7
raregaw <raregaw@wgu.edu>
2025-10-19 21:39:25 -0700

    Step B (revision): Added Map Componenet with SVG World Map Visual Interface

--- interactive-world-map/src/app/app.component.ts
+++ interactive-world-map/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'interactive-world-map';
 }
+
